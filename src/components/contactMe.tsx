3dbbfb0aa38f751692162fc41dcf3d9e4346e62f
Santiago <santimh04@gmail.com>
2025-05-20 11:50:19 -0500

    Formulario listo, previo a añadir funcionalidad

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import HiringContact from './HiringContact';
+import { Notice } from './Notice';
 import Footer from './Footer';
 
 import './contactMe.css'
@@ -9,10 +9,25 @@ function ContactMe() {
     return (
         <section className="mainBody">
             <Header />
+            <Notice metier="Hire me" />
             <section className="contact-me-info">
-
+                <div className="contact-me-title">
+                    <h2>Coding inquiry</h2>
+                    Want to get in touch? Reach out now
+                </div>
+                <form className="contact-me-form">
+                    <div className="from-info">
+                        <input type="text" id="name" value="" placeholder="Name"></input>
+                        <input type="text" id="email" value="" placeholder="Email address"></input>
+                    </div>
+                    <div>
+                        <textarea rows="10" id="message" value="" placeholder="message"></textarea>
+                    </div>
+                    <button type="submit">
+                        Submit
+                    </button>
+                </form>
             </section>
-            <HiringContact />
             <Footer />
         </section>
     );
